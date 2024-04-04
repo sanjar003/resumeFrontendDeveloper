aba9f4d3a46f492a655ca33b6a6ee246c4ec8638
sanjar003 <sanjarsamatov0306@gmail.com>
2024-04-04 23:23:53 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import {BrowserRouter as Route, Router, Routes } from "react-router-dom";
 import "./App.css";
-import Route from "./Routes/Routes";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+// import Route from "./Routes/Routes";
 
 function App() {
   return (
     <div className="App">
-      <Route />
+      {/* <Route /> */}
+      <Router>
+        <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
