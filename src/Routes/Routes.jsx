4048520db93cb5a78c2a28d7113fbcdce47e52f0
sanjar003 <sanjarsamatov0306@gmail.com>
2024-04-04 22:08:25 +0600

    delete

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Route, Router, Switch } from 'react-router-dom'
+import {BrowserRouter as Route, Router } from 'react-router-dom'
 import Home from '../components/Home/Home'
 import About from '../components/About/About'
 
@@ -7,10 +7,8 @@ const Routes = () => {
   return (
     <div>
         <Router>
-            <Switch>
             <Route exact path='/' component={Home}/>
             <Route path='/about' component={About}/>
-            </Switch>
         </Router>
     </div>
   )
