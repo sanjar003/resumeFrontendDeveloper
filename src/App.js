8a25250152d5b4350caf965ac6fb118c8d6d6e21
sanjar003 <sanjarsamatov0306@gmail.com>
2024-04-05 12:19:44 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-// import {Link, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
-// import Home from "./components/Home/Home";
-// import About from "./components/About/About";
-// import Route from "./Routes/Routes";
 
 function App() {
   return (
     <div className="App">
-      {/* <header>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-      </header> */}
-      {/* <Route /> */}
-      {/* <Router>
-        <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        </Routes>
-      </Router> */}
       <h2>fewq</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
