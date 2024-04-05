f8e6e97a337c1d3265633792e18e9dbe2f8f946a
sanjar003 <sanjarsamatov0306@gmail.com>
2024-04-05 12:44:25 +0600

    add

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import { Link, Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/about">about</Link>
+        <Link to="*">not</Link>
+      </header>
+      <Outlet/>
+      <footer>2222</footer>
+    </>
+  );
+};
+
+export default Layout;
