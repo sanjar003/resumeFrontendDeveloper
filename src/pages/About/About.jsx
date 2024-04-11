46a0e629809bbf138fa8580d4be3426c1966f181
sanjar003 <sanjarsamatov0306@gmail.com>
2024-04-11 09:53:43 +0600

    add

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,14 +4,29 @@ import styled from "styled-components";
 const About = () => {
   return (
     <Container>
-      about 
+      <Divcontainer>ere</Divcontainer>
+      <DIVCOntainer>vewvw</DIVCOntainer>
     </Container>
   );
 };
 
 const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   height: 700px;
+  flex-wrap: wrap;
+  gap: 55px;
   background: #303030;
 `;
-
+const Divcontainer = styled.div`
+  width: 400px;
+  height: 400px;
+  background: red;
+`;
+const DIVCOntainer = styled.div`
+  width: 400px;
+  height: 400px;
+  background: blue;
+`;
 export default About;
