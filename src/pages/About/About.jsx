6aa201e824defc42e69f6d96ad72df267edfe3f3
sanjar003 <sanjarsamatov0306@gmail.com>
2024-04-11 21:15:22 +0600

    add-css

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -19,24 +19,39 @@ const About = () => {
       />
       <Divcontainer>
         <b>
-          Мои навыки включают разработку веб-приложений{" "}
-          <ins>использованиеREST API</ins> для обмена данными работу с системой
-          контроля версий Git, создание компонентов с использованием библиотеки{" "}
-          <ins>ReactJs</ins> , а также работу с различными фреймворками и
-          библиотеками такими как <ins>Redux</ins> ,<ins>Mobx</ins> , и
-          <ins>TRK Query,</ins> . Я также опытен в создании адаптивных
-          интерфейсов с помощью <ins>CSS Modules</ins> , и
-          <ins>Styled Components</ins> . Кроме того я знаком с инструментами
-          которые помогают в создании приложений такими как <ins>Docker</ins> ,
-          <ins>Firebase и Axios,</ins> что позволяет мне разрабатывать более
-          масштабируемые и быстрые приложения. В целом я считаю себя опытным
-          <ins>Frontend</ins> , разработчиком который всегда готов принять новые
-          вызовы и научиться новым навыкам чтобы создавать качественные и
-          инновационные веб-приложения.
+          Мои навыки включают разработку веб-приложений
+          <ins style={{ color: "red", fontSize: "22px " }}>
+            использованиеREST API
+          </ins>
+          для обмена данными работу с системой контроля версий{" "}
+          <ins style={{ color: "red", fontSize: "22px" }}>Git</ins> , создание
+          компонентов с использованием библиотеки{" "}
+          <ins style={{ color: "red", fontSize: "22px" }}>ReactJs</ins> , а
+          также работу с различными фреймворками и библиотеками такими как{" "}
+          <ins style={{ color: "red", fontSize: "22px" }}>Redux</ins> ,
+          <ins style={{ color: "red", fontSize: "22px" }}>Mobx</ins> , и
+          <ins style={{ color: "red", fontSize: "22px" }}>TRK Query,</ins> . Я
+          также опытен в создании адаптивных интерфейсов с помощью
+          <ins style={{ color: "red", fontSize: "22px" }}>CSS Modules</ins> , и
+          <ins style={{ color: "red", fontSize: "22px" }}>
+            Styled Components
+          </ins>
+          . Кроме того я знаком с инструментами которые помогают в создании
+          приложений такими как{" "}
+          <ins style={{ color: "red", fontSize: "22px" }}>Docker</ins> ,
+          <ins style={{ color: "red", fontSize: "22px" }}>
+            Firebase и Axios,
+          </ins>{" "}
+          что позволяет мне разрабатывать более масштабируемые и быстрые
+          приложения. В целом я считаю себя опытным
+          <ins style={{ color: "red", fontSize: "22px" }}>Frontend</ins> ,
+          разработчиком который всегда готов принять новые вызовы и научиться
+          новым навыкам чтобы создавать качественные и инновационные
+          <ins style={{ color: "red", fontSize: "22px" }}>веб-приложения</ins>.
         </b>
       </Divcontainer>
       <DIVCOntainer>
-        <img src={Asests} style={{ width: "570px", height: "530px" }} />
+        <img src={Asests} style={{ width: "620px", height: "580px" }} />
       </DIVCOntainer>
     </Container>
   );
@@ -51,13 +66,14 @@ const Container = styled.div`
   gap: 55px;
 `;
 const Divcontainer = styled.div`
-  width: 530px;
+  width: 570px;
   height: 460px;
-
-  font-size: 20px;
+  color: white;
+  text-align: center;
+  font-size: 21px;
 `;
 const DIVCOntainer = styled.div`
-  width: 570px;
-  height: 530px;
+  width: 620px;
+  height: 580px;
 `;
 export default About;
