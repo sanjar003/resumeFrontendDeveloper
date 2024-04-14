60cbd175000b861d5ed24d2a22ed12311cc75348
sanjar003 <sanjarsamatov0306@gmail.com>
2024-04-14 20:46:29 +0600

    add-animation

--- src/pages/About/MySkills.jsx
+++ src/pages/About/MySkills.jsx
@@ -23,74 +23,74 @@ function MySkills() {
   return (
     <Container>
       <>
-        <ChildrenCOntainer>
+        <ChildrenContainer>
           <Col xs={4} md={2} className="tech-icons">
             <DiHtml5 />
           </Col>
-        </ChildrenCOntainer>
-        <ChildrenCOntainer>
+        </ChildrenContainer>
+        <ChildrenContainer>
           <Col xs={4} md={2} className="tech-icons">
             <DiCss3 />
           </Col>
-        </ChildrenCOntainer>
-        <ChildrenCOntainer>
+        </ChildrenContainer>
+        <ChildrenContainer>
           <Col xs={4} md={2} className="tech-icons">
             <DiJavascript1 />
           </Col>
-        </ChildrenCOntainer>
-        <ChildrenCOntainer>
+        </ChildrenContainer>
+        <ChildrenContainer>
           <Col xs={4} md={2} className="tech-icons">
             <DiReact />
           </Col>
-        </ChildrenCOntainer>
+        </ChildrenContainer>
 
-        <ChildrenCOntainer>
+        <ChildrenContainer>
           <Col xs={4} md={2} className="tech-icons">
             <SiRedux />
           </Col>
-        </ChildrenCOntainer>
+        </ChildrenContainer>
 
-        <ChildrenCOntainer>
+        <ChildrenContainer>
           <Col xs={4} md={2} className="tech-icons">
             <SiFirebase />
           </Col>
-        </ChildrenCOntainer>
-        <ChildrenCOntainer>
+        </ChildrenContainer>
+        <ChildrenContainer>
           <Col xs={4} md={2} className="tech-icons">
             <DiGit />
           </Col>
-        </ChildrenCOntainer>
-        <ChildrenCOntainer>
+        </ChildrenContainer>
+        <ChildrenContainer>
           <Col xs={4} md={2} className="tech-icons">
             <DiGithub />
           </Col>
-        </ChildrenCOntainer>
-        <ChildrenCOntainer>
+        </ChildrenContainer>
+        <ChildrenContainer>
           <Col xs={4} md={2} className="tech-icons">
             <SiFigma />
           </Col>
-        </ChildrenCOntainer>
-        <ChildrenCOntainer>
+        </ChildrenContainer>
+        <ChildrenContainer>
           <Col xs={4} md={2} className="tech-icons">
             <DiNodejs />
           </Col>
-        </ChildrenCOntainer>
-        <ChildrenCOntainer>
+        </ChildrenContainer>
+        <ChildrenContainer>
           <Col xs={4} md={2} className="tech-icons">
             <SiTypescript />
           </Col>
-        </ChildrenCOntainer>
-        <ChildrenCOntainer>
+        </ChildrenContainer>
+        <ChildrenContainer>
           <Col xs={4} md={2} className="tech-icons">
             <FaSass />
           </Col>
-        </ChildrenCOntainer>
+        </ChildrenContainer>
 
-        <ChildrenCOntainer>
+        <ChildrenContainer>
           <Col xs={4} md={2} className="tech-icons">
             <SiAdobephotoshop />
           </Col>
-        </ChildrenCOntainer>
+        </ChildrenContainer>
       </>
     </Container>
   );
@@ -103,12 +103,29 @@ const Container = styled.div`
   flex-wrap: wrap;
   font-size: 90px;
 `;
-const ChildrenCOntainer = styled.div`
+const ChildrenContainer = styled.div`
   width: 159px;
   height: 149px;
   cursor: pointer;
   font-size: 1.5em;
   border: 2px solid black;
   text-align: center;
+
+  &:hover {
+    box-shadow:
+  2.3px 2.3px 1.9px rgba(0, 0, 0, 0.248),
+  5.4px 5.4px 4.3px rgba(0, 0, 0, 0.289),
+  9.7px 9.7px 7.7px rgba(0, 0, 0, 0.312),
+  16.1px 16.1px 12.8px rgba(0, 0, 0, 0.328),
+  26.5px 26.5px 21.2px rgba(0, 0, 0, 0.34),
+  46.2px 46.2px 37px rgba(0, 0, 0, 0.351),
+  100px 100px 80px rgba(0, 0, 0, 0.36)
+;
+    border: 1.9px solid white;
+    /* transform: translate(1px, 1px);  */
+    transition-delay:'2s'
+    
+    }
 `;
+
 export default MySkills;
