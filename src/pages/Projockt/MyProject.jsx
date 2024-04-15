74a2886ecf0f00eb488aa755c2f44d0972572243
sanjar003 <sanjarsamatov0306@gmail.com>
2024-04-15 20:19:01 +0600

    add-icons

--- src/pages/Projockt/MyProject.jsx
+++ src/pages/Projockt/MyProject.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const MyProject = () => {
-  return <Container>MyProject</Container>;
+  return (
+    <Container>
+      <a href="https://github.com/dashboard">
+        <i class="bi bi-github"></i>
+      </a>
+    </Container>
+  );
 };
 
 const Container = styled.div`
   max-height: 100%;
   width: 100%;
   background: red;
-  padding-top:90px;
+  padding-top: 90px;
 `;
+
 
 export default MyProject;
