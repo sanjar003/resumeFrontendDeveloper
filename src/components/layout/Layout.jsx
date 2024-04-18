d6a1d60a41ccfe4cfba2769007190a8bfadb432d
sanjar003 <sanjarsamatov0306@gmail.com>
2024-04-18 23:17:43 +0600

    add-components

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,47 +1,36 @@
 import { Link, Outlet } from "react-router-dom";
 import styled from "styled-components";
-// import Footer from "../../pages/footer/Footer";
 
 const Layout = () => {
   return (
     <>
-      <Headers>
-        <div>
-          <Link to="/">
-            <Span>Home</Span>
-          </Link>
-
-          <Link to="/about">
-            <Span>About Me</Span>
-          </Link>
-        </div>
-        {/* <div>
-          <Link to="project">
-            <Span>MyProject</Span>
-          </Link>
-        </div> */}
-      </Headers>
+      <div>
+        <Link to="/">
+          <Span>Home</Span>
+        </Link>
+        <Headers>
+          <>
+            <Link to="/about">
+              <Span>About Me</Span>
+            </Link>
+          </>
+        </Headers>
+      </div>
       <Outlet />
-      {/* <footer>2222</footer> */}
     </>
   );
 };
 
 const Headers = styled.header`
   display: flex;
-  justify-content: center;
-  align-items: center;
   position: fixed;
-  width: 100%;
+  width: 90%;
   height: 50px;
-  /* color: black; */
-  /* background: #8121d0; */
   backdrop-filter: blur(2px);
 `;
 const Span = styled.span`
   margin-top: 6px;
   font-size: 22px;
-  margin-left: 15px;
   color: #0a0a0a;
 `;
 
